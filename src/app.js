ab7c6b6f8334189fd4ca68f404d0e96e8e7a8901
TPetrica <petru.tirla1@gmail.com>
2025-01-13 21:21:27 +0100

    Fix frontend url cors

--- src/app.js
+++ src/app.js
@@ -68,6 +68,7 @@ const corsOptions = {
     'http://localhost:5173',
     'http://localhost:3000',
     'https://checkout.stripe.com',
+    'https://elitetransportationpc.com',
     /\.stripe\.com$/,
     config.clientUrl,
   ],
